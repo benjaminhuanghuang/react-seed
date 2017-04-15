37aa3ce91f8bb7c55ecb9cbcb6dd48240309d654
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-14 22:46:14 -0700

    state

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import './app.css'
 
 class App extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017',
+            counter: 0
+        };
+    }
+
+    changeDeadline(){
+        this.setState({deadline: "November 25, 2017"});
+    }
     render(){
-        return (<div> Test</div>)
+        return (<div className="app">  
+                    <div className="app-title"> Countdown to {this.state.deadline}</div>
+                    <div> 
+                        <div className="time"> Days </div>
+                        <div className="time"> Hours </div>
+                        <div className="time"> minutes</div>    
+                        <div className="time"> seconds</div>    
+                    </div>
+                    <div>
+                        <input type="text" placeholder='new date'/>
+                        <button onClick={()=>this.changeDeadline()}>Submit</button>
+                    </div>
+                </div>) 
     }
 }
 
