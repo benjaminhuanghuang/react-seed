76e22e09e1bfe354fa3077bf1bc39d023a6a4b76
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-15 19:20:01 -0700

    update

--- src/music_master/app.jsx
+++ src/music_master/app.jsx
@@ -1,26 +1,39 @@
-import React, {Component} from 'react';
-import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
- 
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
 import './app.css'
 
-class App extends Component{
-    constructor(props)
-    {
+class App extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-  
-        }; 
+            query: ''
+        };
     }
 
-    render(){
-        return (<div className="app">  
+    search() {
+        const BASE_URL = 'https://api.spotify.com/v1/search';
+        const FETCH_URL = BASE_URL + "q=" + this.state.query + "&type=artist&limit=1"
+        console.log(FETCH_URL);
+    }
+    render() {
+        return (<div className="app">
                     <div className="app-title">Music Master</div>
                     <FormGroup>
                         <InputGroup>
-                              <FormControl type= "text" placeholder="search an artist..."/>
-                              <InputGroup.Addon>
+                            <FormControl type="text" placeholder="search an artist..."
+                                query={this.state.query}
+                                onChange={event=>{ this.setState({ query: event.target.value }) }}
+                                onKeyPress={event=>{
+                                    if(event.key === "Enter")
+                                    {
+                                        this.search();
+                                    }
+                                }}
+                            />
+                            <InputGroup.Addon onClick={() => this.search()}>
                                 <Glyphicon glyph="search"></Glyphicon>
-                              </InputGroup.Addon>
+                            </InputGroup.Addon>
                         </InputGroup>
                     </FormGroup>
                     <div className="profile">
@@ -30,8 +43,8 @@ class App extends Component{
                     <div className="gallerry">
                         Gallery
                     </div>
-                </div>) 
+            </div>)
     }
 }
- 
+
 export default App;
